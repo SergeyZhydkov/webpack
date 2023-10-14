c76e2f0381e98807cc0ecf5426fe98797a8a0452
abtsty <andrey.barylov@gmail.com>
2023-10-14 12:03:40 +0300

    webpack

--- src/index.js
+++ src/index.js
@@ -31,5 +31,63 @@
 
 // console.log(sum(2, 3));
 
+import "./styles/style.css";
 
-import './styles/style.css'
+// СТВОРИ СПИСОК ЗАМІТОК НА ДЕНЬ:
+// 1)ПОЛУЧИТИ ДОСТУП ДО ЕЛЕМЕНТІВ ФОРМИ, ПРИ НАТИСКАННЯ НА КНОПОЧКУ ADD
+
+// 2)НА ОСНОВІ ДАНИХ ЯКІ МИ ВЗЯЛИ З ФОРМИ ВІДМАЛЮВАТИ
+// ЕЛЕМЕНТИ СПИСКУ НА ЕКРАН
+
+// 3)ДОДАЙ ЦЕЙ СПИСОК ДО ЛОКАЛ СТОРЕДЖ
+
+// 4)ДОДАЙ ДОДАТКОВИЙ ФУНКЦІОНАЛ, ЩОБ ПРИ ОНОВЛЕННІ СТОРІНКИ СПИСОК НЕ ВИДАЛЯВСЯ
+
+// 5)СТВОРЮЄМО КНОПОЧКУ, ПРИ ЯКОМУ БУДЕ ОЧИЩАВСЯ ЛОКАЛ СТОРЕДЖ
+
+const form = document.querySelector("#form");
+const input = document.querySelector("#input");
+const list = document.querySelector(".js-list");
+const clearBtn = document.querySelector(".clearBtn");
+
+form.addEventListener("submit", inputValue);
+
+function inputValue(evt) {
+    evt.preventDefault();
+
+    let value = input.value;
+    const liElem = document.createElement("li");
+
+    liElem.textContent = value;
+    list.appendChild(liElem);
+
+    const localStorageParsed = JSON.parse(localStorage.getItem("liElem")) || [];
+    localStorageParsed.push(value);
+    updateLocalStorage(localStorageParsed);
+
+    input.value = "";
+}
+
+function updateLocalStorage(liElem) {
+    localStorage.setItem("liElem", JSON.stringify(liElem));
+}
+
+function savedValue() {
+    const savedItems = localStorage.getItem("liElem");
+    if (savedItems) {
+        const newParsed = JSON.parse(savedItems);
+        for (const element of newParsed) {
+            const liElem = document.createElement("li");
+            liElem.textContent = element;
+            list.appendChild(liElem);
+        }
+    }
+}
+savedValue();
+
+clearBtn.addEventListener("click", storageClear);
+
+function storageClear() {
+    localStorage.removeItem("liElem");
+    list.innerHTML = "";
+}
